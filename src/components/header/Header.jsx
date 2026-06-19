228f02c45246436e5f42c9b315a8707cbeda5070
mahimaanchra <mahimaanchra@gmail.com>
2026-06-19 09:44:17 -0700

    added myPost to navItems

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,9 @@ function Header() {
     { name: 'Login', slug: "/login", active: !authStatus },
     { name: 'Signup', slug: "/signup", active: !authStatus },
     { name: 'All Posts', slug: "/all-posts", active: authStatus },
-    { name: 'Add Post', slug: "/add-post", active: authStatus }
+    { name: 'Add Post', slug: "/add-post", active: authStatus },
+    {name: 'My Posts', slug: "/my-posts", active: authStatus}
+    
   ];
 
   return (
